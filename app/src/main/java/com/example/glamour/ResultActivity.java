4cc100a945dbb88f3dbb4b9996908b71d7425d59
raj singh <rajsingh2512@outlook.com>
2021-08-10 13:00:39 +0200

    my project

---
+++ app/src/main/java/com/example/glamour/ResultActivity.java
@@ -0,0 +1,23 @@
+package com.example.glamour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.glamour.databinding.ActivityResultBinding;
+
+public class ResultActivity extends AppCompatActivity {
+
+    ActivityResultBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityResultBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.image.setImageURI(getIntent().getData());
+    }
+}
